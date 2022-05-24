3c25c7b1d396e3719281adeb3163775471aa65fc
bzz <viruthiyelv@bzz.ch>
2022-05-24 19:30:44 +0200

    Milestone 1 completed with testcases and json/properties files

--- src/main/java/ch/bzz/footballTeam/service/TeamService.java
+++ src/main/java/ch/bzz/footballTeam/service/TeamService.java
@@ -13,7 +13,6 @@ import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Path("team")
 public class TeamService {
