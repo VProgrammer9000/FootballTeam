754bd4491824ec2b456bf9e82f126ecc4236e84d
bzz <viruthiyelv@bzz.ch>
2022-06-26 17:50:28 +0200

    login works

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,34 @@
+/**
+ * view-controller for login-form
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("loginForm").addEventListener("submit", loginUser);
+});
+
+function loginUser(event) {
+    event.preventDefault();
+    showMessage("", "info");
+
+    const loginForm = document.getElementById("loginForm");
+    const formData = new FormData(loginForm);
+    const data = new URLSearchParams(formData);
+
+    fetch("./resource/user/login",
+        {method: "POST", headers: {"Content-Type": "application/x-www-form-urlencoded"}, body: data})
+        .then(function (response) {
+            if (!response.ok) {
+                showMessage("Username/Password unknown", "error");
+            } else loginSuccess()
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+/**
+ * save the JWToken and redirect
+ * @param response
+ */
+function loginSuccess() {
+    window.location.href = "./gameList.html";
+}
